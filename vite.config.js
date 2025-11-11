f2d6ded305c5069650035602aea95c141970ece4
xandre55 <chideraxandre55@gmail.com>
2025-11-11 04:48:01 +0100

    another changes

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/xandre-frontend/', // Important for GitHub Pages
+  base: '/xandre-frontend', // Important for GitHub Pages
   plugins: [react()]
 })
 
